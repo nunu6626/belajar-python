8dce769a3ca4256b8087ced83da1154ca7b585d7
nunu6626 <nutami006@gmail.com>
2023-08-01 16:53:04 +0800

    menggabungkan dua karakter

---
+++ huruf.py
@@ -0,0 +1,20 @@
+kata1 = input("masukan sebuah kata: ")
+
+if "a" in kata1:
+    print("terdapat huruf vokal di dalam kata : %s" % (kata1))
+
+elif "o" in kata1:
+    print("terdapat huruf vokal di dalam kata : %s" % (kata1))
+
+elif "i" in kata1:
+    print("terdapat huruf vokal di dalam kata : %s" % (kata1))
+
+elif "u" in kata1:
+    print("terdapat huruf vokal di dalam kata : %s" % (kata1))
+
+elif "e" in kata1:
+    print("terdapat huruf vokal di dalam kata : %s" % (kata1))
+
+else:
+    print("tidak terdapat huruf vokal di dalam kata : %s" % (kata1))
+
