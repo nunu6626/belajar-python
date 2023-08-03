25f166fbb5e051f94f242140968e5b66368a525d
nunu6626 <nutami006@gmail.com>
2023-08-03 17:27:48 +0800

    control-flow

--- control-flow.py
+++ control-flow.py
@@ -1,7 +1,30 @@
-x = 89
+
+x = 75
 if x  >= 90 : 
     print("nilai {nilai} = grade A".format(nilai = x))
 elif x >= 85 and x <= 90 : 
-    print("nilai {nilai} = grade B".format(nilai = x))
+     print("nilai {nilai} = grade B".format(nilai = x))
+ elif x >= 80 and x <= 85 : 
+     print("nilai {nilai} = grade C".format(nilai = x))
+elif x >= 70 and x <= 80 : 
+     print("nilai {nilai} = grade D".format(nilai = x))
 else:
-    print("nilai {nilai} = grade E ".format(nilai = x))
+    print("nilai {nilai} = grade E".format(nilai = x)) 
+
+
+if harga  == 2000 and harga == 3000
+    print("jika harga kangkung {jikahargakangkung} = beli 3 ikat".format(jikahargakangkung = harga))
+elif harga == 2000 and harga == 3000 : 
+    print("jika harga kangkung {jikahargakangkung} = beli 2 ikat".format(jikahargakangkung = harga))
+
+else:
+    print("jika harga kangkung {jikahargakangkung} = beli 1 ikat ".format(jikahargakangkung = harga))
+
+
+harga = 3000
+
+if harga == 2000 and harga == 3000:
+    print("beli 3 ikat")
+else:
+    print("beli 2 ikat")
+
