4fe1d3d488c1f6282eaae9ab1e019315e16aa350
nunu6626 <nutami006@gmail.com>
2023-08-03 16:31:13 +0800

    belajar control-flow

---
+++ control-floww.py
@@ -0,0 +1,9 @@
+harga = 2000
+
+if harga  >= 3000 :
+    print("jika harga kangkung {jikahargakangkung} = 1 ikat".format(jikahargakangkung = harga))
+elif harga >= 2000 and harga <= 2000 : 
+    print("jika harga kangkung {jikahargakangkung} = 2 ikat".format(jikahargakangkung = harga))
+
+else:
+    print("jika harga kangkung {jikahargakangkung} = 2 ikat ".format(jikahargakangkung = harga))
