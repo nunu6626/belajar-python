2e56771d30b24534f504c6c45e3687d2903e9703
nunu6626 <nutami006@gmail.com>
2023-08-04 15:54:28 +0800

    belajar aritmatika.py

---
+++ aritmatika.py
@@ -0,0 +1,44 @@
+# nomber
+x = 100
+y = 100
+
+z = x * y
+print("nilai z adalah:", z)
+
+n = 10
+r = 20
+
+v = n - r
+print("nilai v adalah:", v)
+
+o = 30
+p = 10
+
+e = o + p
+print("nilai e adalah:", e)
+
+k = 50
+m = 100
+
+s = k / m
+print("nilai s adalah:", s)
+
+z = 2
+p = 10
+
+a = z % p
+print("nilai a adalah:" , a)
+
+
+
+#string
+y = 5
+x = "10"
+
+f = y * x
+print("nilai f adalah:" , f)
+
+# float
+
+a= round(17, -1)
+print("nilai a adalah:" ,a)
