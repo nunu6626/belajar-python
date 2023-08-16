905e7273f1ece0bc3645bbd604ba6e8102c820d6
nunu6626 <nutami006@gmail.com>
2023-08-16 12:03:26 +0800

    belajar index

---
+++ list.py
@@ -0,0 +1,30 @@
+
+# program untuk cari angka 3 ada di urutan atau index berapa
+
+x = [1,2,3,4,5]
+y = 6
+if y in x :
+    print ("angka 3 ada pada index ke : ",x.index(3))
+else :
+    print("angka 3 tidak ada pada array")
+
+# program untuk menghapus item dari array/list
+
+x = [1,2,3,4,5]
+x.remove((4))
+print(x)
+
+
+test_list = [1,2,3,4,5]
+
+test_list.pop(3)
+
+print(test_list)
+
+
+
+
+
+
+
+
